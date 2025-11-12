70ce5dd3ba0cd5e4690604000bb3f858343fba5e
glezgroove <motresku.vladislav9@gmail.com>
2025-11-13 00:41:40 +0300

    added homework

---
+++ Homework_12_11/ConsoleApp/ConsoleApp.cs
@@ -0,0 +1,53 @@
+﻿namespace ConsoleApp
+{
+    public class ConsoleApp
+    {
+        public static IEnumerable<int> GetOdd(ICollection<int> nums)
+        {
+            List<int> result = new List<int>();
+            foreach (int i in nums)
+            {
+                if (i % 2 != 0)
+                    result.Add(i);
+            }
+            return result;
+        }
+        
+        public static List<int> InputHandler()
+        {
+            List<int> list = new List<int> ();
+            string? number;
+            Console.WriteLine("Введите необходимые числа. Для расчёта введите что угодно, кроме целого числа.");
+            while (true)
+            {
+                try
+                {
+                    number = Console.ReadLine();
+                    {
+                        if (!int.TryParse(number, out int parsedNum))
+                        {
+                            Console.WriteLine("Введено не целое число. Вывод нечётных чисел");
+                            break;
+                        }
+                        Console.WriteLine($"Ваше число {number}");
+                        list.Add(parsedNum);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+            var UserInputList = InputHandler();
+            var result = GetOdd(UserInputList);
+            foreach (int number in result)
+                Console.WriteLine(number);
+        } 
+    } 
+}
---
+++ Homework_12_11/Unit_Test/Unit_Test.cs
@@ -0,0 +1,88 @@
+﻿namespace Unit_Test;
+
+public class UnitTest
+{
+    
+    [Fact]
+    public void GetOdd_Standart()
+    {
+        //Arrange
+        var input = new List<int>{1,2,5,10,15,25,125,9999,2165221,2147483647};
+        var expected = new List<int>{1,5,15,25,125,9999,2165221,2147483647};
+
+        //Act
+        var result = ConsoleApp.ConsoleApp.GetOdd(input);
+
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    [Fact]
+    public void GetOdd_NoNumbers()
+    {
+        //Arrange
+        var input = new List<int>();
+        var expected = new List<int>();
+
+        //Act
+        var result = ConsoleApp.ConsoleApp.GetOdd(input);
+
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetOdd_NegativeAndPositive()
+    {
+        //Arrange
+        var input = new List<int>{9,2,1,0,-1,-2,-3,-2147483647};
+        var expected = new List<int>{9,1,-1,-3,-2147483647};
+
+        //Act
+        var result = ConsoleApp.ConsoleApp.GetOdd(input);
+
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetOdd_OnlyOneEven()
+    {
+        //Arrange
+        var input = new List<int>{2};
+        var expected = new List<int>{};
+
+        //Act
+        var result = ConsoleApp.ConsoleApp.GetOdd(input);
+
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetOdd_OnlyOneOdd()
+    {
+        //Arrange
+        var input = new List<int>{9};
+        var expected = new List<int>{9};
+
+        //Act
+        var result = ConsoleApp.ConsoleApp.GetOdd(input);
+
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetOdd_MultpleZeroes()
+    {
+        //Arrange
+        var input = new List<int>{0000000,-0000000};
+        var expected = new List<int>{};
+
+        //Act
+        var result = ConsoleApp.ConsoleApp.GetOdd(input);
+
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+}
