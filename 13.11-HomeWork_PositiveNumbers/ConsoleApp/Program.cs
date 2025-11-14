a646d98ebc44417a4a1ffa195491c9ce3f0c50cb
glezgroove <motresku.vladislav9@gmail.com>
2025-11-14 09:49:32 +0300

    Homework 13_11
    Homework 13_11

---
+++ 13.11-HomeWork_PositiveNumbers/ConsoleApp/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _13_11_HomeWork_ConsoleApp
+{
+    public class HomeworkConsoleApp
+    {
+        public ICollection<int> GetPositiveNumbers(ICollection<int> numbers)
+        {
+            return numbers.Where(i => i > 0).ToList();
+        }
+        
+        public ICollection<int> InputHandler()
+        {
+            List<int> list = new List<int>();
+            string? number;
+            Console.WriteLine("Введите необходимые числа. Для расчёта введите что угодно, кроме целого числа.");
+            while (true)
+            {
+                try
+                {
+                    number = Console.ReadLine();
+                    {
+                        if (!int.TryParse(number, out int parsedNum))
+                        {
+                            Console.WriteLine("Введено не целое число. Вывод положительных чисел");
+                            break;
+                        }
+                        Console.WriteLine($"Ваше число {number}");
+                        list.Add(parsedNum);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
+            return list;
+        }
+
+        static void Main(string[] args)
+        {
+            var consoleApp = new HomeworkConsoleApp();
+            var userInputList = consoleApp.InputHandler();
+            var result = consoleApp.GetPositiveNumbers(userInputList);
+            foreach (int number in result)
+                Console.WriteLine(number);
+        }
+    } 
+}
---
+++ 13.11-HomeWork_PositiveNumbers/Unit-Test/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using _13_11_HomeWork_ConsoleApp;
+
+namespace Unit_Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnNothing_WhenNoNumbersExist()
+    {
+        //Arrange
+        var consoleApp = new HomeworkConsoleApp();
+        var input = new List<int>{};
+        var expected = new List<int>{};
+        
+        //Act
+        var result = consoleApp.GetPositiveNumbers(input);
+        
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnOne_WhenOnePositiveAndMultipleNegative()
+    {
+        //Arrange
+        var consoleApp = new HomeworkConsoleApp();
+        var input = new List<int>{0,-2,-5,-12,-666,-9999,999,-8};
+        var expected = new List<int>{999};
+        
+        //Act
+        var result = consoleApp.GetPositiveNumbers(input);
+        
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnNothing_WhenOnlyNegative()
+    {
+        //Arrange
+        var consoleApp = new HomeworkConsoleApp();
+        var input = new List<int>{-1,-2,-3,-4,-5,-6,-7,-8,-2159120};
+        var expected = new List<int>{};
+        
+        //Act
+        var result = consoleApp.GetPositiveNumbers(input);
+        
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    
+    [Fact]
+    public void GetPositiveNumbers_ShouldReturnSameAsInput_WhenOnlyPositive()
+    {
+        //Arrange
+        var consoleApp = new HomeworkConsoleApp();
+        var input = new List<int>{9999,8888,77777,912311,01,22,5};
+        var expected = new List<int>{9999,8888,77777,912311,1,22,5};
+        
+        //Act
+        var result = consoleApp.GetPositiveNumbers(input);
+        
+        //Arrange
+        Assert.Equal(expected, result);
+    }
+    
+}
